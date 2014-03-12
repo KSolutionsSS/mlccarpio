37eb05b44dc994b7949abf23335dc4061776aa1f
Nahuel Barrios <barrios.nahuel@gmail.com>
2014-03-12 13:41:39 -0300

    Returns index.html configured with Bootstrap and JQuery.

---
+++ public/js/index.js
@@ -0,0 +1,8 @@
+/**
+ * Created by Nahuel Barrios <barrios.nahuel@gmail.com>.
+ * Created on 12/03/14, at 13:34.
+ */
+
+$(document).ready(function () {
+    console.log('JQuery configured successfully.');
+});
